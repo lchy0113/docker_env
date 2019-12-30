50ba48288f2cc1c576eda5f7405f834e6d33b299
changyong Lee <lchy0113@gmail.com>
2019-12-30 16:10:42 +0900

    Add LC_ALL=C.UTF-8

--- richgold.sh
+++ richgold.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it  -v /home/lchy0113:/home/lchy0113 --privileged -v /dev/bus/usb:/dev/bus/usb tcc8985:0.0
+docker run -it  -v /home/lchy0113:/home/lchy0113 --privileged -v /dev/bus/usb:/dev/bus/usb -e LC_ALL=C.UTF-8 tcc8985:0.0
