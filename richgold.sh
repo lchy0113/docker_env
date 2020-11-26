f80bec2d5a7781f220a91d83f1ad8ac0cb7ad106
이창용 (HN연구팀) <lchy0113@gmail.com>
2020-11-26 19:46:56 +0900

    Update docker image.

--- richgold.sh
+++ richgold.sh
@@ -20,12 +20,12 @@ case $1 in
 		echo "------------------------------------------"
 		echo "--> RUN"
 		# docker exec -it Android_Develop bash
-		docker run -it  -v /home/lchy0113:/home/lchy0113 --privileged -v /dev/bus/usb:/dev/bus/usb -v /etc/localtime:/etc/localtime:ro -e LC_ALL=C.UTF-8 tcc8985:0.0
+		docker run -it  -v /home/lchy0113:/home/lchy0113 --privileged -v /dev/bus/usb:/dev/bus/usb -v /etc/localtime:/etc/localtime:ro -e LC_ALL=C.UTF-8 tcc8985:0.3
 		;;
 	build)
 		echo "------------------------------------------"
 		echo "--> BUILD"
-		# docker build --no-cache --tag tcc8985:0.1 .
-		docker build --tag tcc8985:0.1 .
+		docker build --no-cache --tag tcc8985:0.3 .
+		# docker build --tag tcc8985:0.3 .
 		;;
 esac
